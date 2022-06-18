13950fb5cb9f8658f77e808a96e5a7d6c6982672
vot3tada <zindromed@bk.ru>
2022-06-18 23:00:46 +0300

    Добавил аудио, миска риса и некоарк. +100 социального рейтинга☺♥♥♥

---
+++ Assets/Resources/Prefabs/NecoArc/NecoScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NecoScript : MonoBehaviour
+{
+    public Sprite[] sprites;
+    private SpriteRenderer SpriteRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRender = GetComponentInParent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpriteRender.sprite = sprites[(int)(Time.time * 10) % sprites.Length];
+    }
+}
